3c5d8f29bb552724cdb528bffc68188a47ed038c
hannahtabunan-la <hannah.tabunan@lawadvisor.com>
2021-06-12 00:59:39 +0800

    Added seeding for users

--- backend/priv/repo/seeds.exs
+++ backend/priv/repo/seeds.exs
@@ -9,3 +9,10 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+
+alias Backend.Repo
+alias Backend.Schemas.User
+
+# insert default user
+Repo.insert!(%User{username: "hannah.tabunan@lawadvisor.com", name: "hannah", password: "palindrome"})
