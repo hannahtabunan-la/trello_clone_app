ae73cb70d96262f725488c25ad934f55226a54a5
hannahtabunan-la <hannah.tabunan@lawadvisor.com>
2021-06-13 03:24:11 +0800

    Fixd typo

--- backend/lib/backend/schemas/user.ex
+++ backend/lib/backend/schemas/user.ex
@@ -7,7 +7,7 @@ defmodule Backend.Schemas.User do
     field :password, :string, virtual: true
     field :encrypted_password, :string
     field :username, :string
-    has_many :board, EctoAssoc.Board  # Board relationship
+    has_many :boards, EctoAssoc.Board  # Board relationship
 
     timestamps()
   end
