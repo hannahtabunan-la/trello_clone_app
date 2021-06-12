21bb4444bafd044c5b76b0f2ddbbf32b737f099a
hannahtabunan-la <hannah.tabunan@lawadvisor.com>
2021-06-12 23:47:13 +0800

    Added index, show and update functions in user controller

--- backend/lib/backend_web/controllers/user_controller.ex
+++ backend/lib/backend_web/controllers/user_controller.ex
@@ -23,4 +23,24 @@ defmodule BackendWeb.UserController do
       |> render("user.json", %{user: user, token: token})
     end
   end
+
+  def index(conn, _params) do
+    users = Users.list_users()
+    conn
+    |> put_status(:ok)
+    |> render("index.json", users: users)
+  end
+
+  def show(conn, %{"id" => id}) do
+    user = Users.get_user!(id)
+    render(conn, "show.json", user: user)
+  end
+
+  def update(conn, %{"id" => id, "user" => user_params}) do
+    user = Users.get_user!(id)
+
+    with {:ok, %User{} = user} <- Users.update_user(user, user_params) do
+      render(conn, "show.json", user: user)
+    end
+  end
 end
