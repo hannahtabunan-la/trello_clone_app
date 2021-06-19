a345a8137aac6e1c1c32547f159a65d05300fcbe
hannahtabunan-la <hannah.tabunan@lawadvisor.com>
2021-06-20 00:13:34 +0800

    [F] Added Auth API hanlder

---
+++ frontend/lib/frontend/api/auth.ex
@@ -0,0 +1,31 @@
+defmodule Frontend.API.Auth do
+  use Tesla
+
+  alias Ecto.Changeset
+
+  alias Frontend.Schemas.User
+  alias Frontend.API.Session
+
+  @success_codes 200..399
+
+  def auth(params) do
+
+  end
+
+  defp from_response(%{"user"=>user, "token"=>token}) do
+    changeset = %User{}
+    |> User.changeset(user)
+    |> Changeset.apply_changes()
+
+    %{"user": changeset, "token": token }
+  end
+
+  def client() do
+    middleware = [
+      {Tesla.Middleware.BaseUrl, "http://localhost:4000/api"},
+      Tesla.Middleware.JSON
+    ]
+
+    Tesla.client(middleware)
+  end
+end
