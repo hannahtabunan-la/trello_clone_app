0d7f2340208f121e255131adfea784ca58344e5b
hannahtabunan-la <hannah.tabunan@lawadvisor.com>
2021-06-20 00:11:46 +0800

    [F] Fixed user schema

--- frontend/lib/frontend/schemas/user.ex
+++ frontend/lib/frontend/schemas/user.ex
@@ -3,52 +3,43 @@ defmodule Frontend.Schemas.User do
   import Ecto.Changeset
 
   @schema_fields [
-    :user_id,
+    :id,
     :name,
-    :password,
-    :encrypted_password,
-    :email
+    :email,
+    :inserted_at,
+    :updated_at
   ]
 
-  @derive {Phoenix.Param, key: :user_id}
+  # @derive {Phoenix.Param, key: :user_id}
   @derive {Jason.Encoder, only: @schema_fields}
 
   @primary_key false
   schema "users" do
-    field :user_id, Ecto.UUID, primary_key: true
+    field :user_id, :id
     field :name, :string
     field :password, :string, virtual: true
-    field :encrypted_password, :string
     field :email, :string
 
     timestamps()
   end
 
   @doc false
-  def changeset(user, attrs) do
+  def changeset(struct, params \\ %{}) do
+    cast(struct, params, @schema_fields)
+  end
+
+  def create_changeset(user, attrs) do
     user
     |> cast(attrs, [:email, :name, :password])
     |> validate_required([:email, :name, :password])
     |> validate_format(:email, ~r/^[A-Za-z0-9._-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/)
     |> unique_constraint(:email)
     |> validate_length(:password, min: 6)
-    |> put_hashed_password
   end
 
   def signin_changeset(user, attrs) do
     user
     |> cast(attrs, [:email, :password])
     |> validate_required([:email, :password])
-    |> put_hashed_password
-  end
-
-  defp put_hashed_password(changeset) do
-    case changeset do
-      %Ecto.Changeset{valid?: true, changes: %{password: password}}
-        ->
-          put_change(changeset, :encrypted_password, Comeonin.Bcrypt.hashpwsalt(password))
-      _ ->
-          changeset
-    end
   end
 end
