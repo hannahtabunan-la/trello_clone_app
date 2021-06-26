02f45abfac506824087896a81b080ddd769119ff
hannahtabunan-la <hannah.tabunan@lawadvisor.com>
2021-06-26 18:26:00 +0800

    [B] Added List schema, model, controller and migration

---
+++ backend/lib/backend/schemas/list.ex
@@ -0,0 +1,45 @@
+defmodule Backend.Schemas.List do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @schemas [
+    :title,
+    :position,
+    :is_deleted,
+    :user_id,
+    :board_id
+  ]
+
+  @required [
+    :title
+  ]
+
+  schema "lists" do
+    field :is_deleted, :boolean, default: false
+    field :title, :string
+    field :position, :decimal, default: 1.0
+    belongs_to :board, Backend.Schemas.Board  # Board relationship
+    belongs_to :user, Backend.Schemas.User # User relationship
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(list, attrs) do
+    list
+    |> cast(attrs, @schemas)
+    |> validate_required(@required)
+  end
+
+  def create_changeset(list, attrs) do
+    list
+    |> cast(attrs, @schemas)
+    |> validate_required(@required)
+  end
+
+  def update_changeset(list, attrs) do
+    list
+    |> cast(attrs, [:title, :position, :is_deleted])
+    |> validate_required(@required)
+  end
+end
---
+++ backend/priv/repo/migrations/20210625153520_create_lists.exs
@@ -0,0 +1,12 @@
+defmodule Backend.Repo.Migrations.CreateLists do
+  use Ecto.Migration
+
+  def change do
+    create table(:lists) do
+      add :title, :string
+
+      timestamps()
+    end
+
+  end
+end
