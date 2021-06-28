1001068aacfb9d39c927241c2387c5b031854eba
hannahtabunan-la <hannah.tabunan@lawadvisor.com>
2021-06-28 20:02:49 +0800

    [B] Updated permission view

--- backend/lib/backend_web/views/permission_view.ex
+++ backend/lib/backend_web/views/permission_view.ex
@@ -1,6 +1,8 @@
 defmodule BackendWeb.PermissionView do
   use BackendWeb, :view
   alias BackendWeb.PermissionView
+  # alias BackendWeb.UserView
+  # alias BackendWeb.BoardView
 
   def render("index.json", %{permissions: permissions}) do
     # %{data: render_many(permissions, PermissionView, "permission.json")}
@@ -9,7 +11,7 @@ defmodule BackendWeb.PermissionView do
 
   def render("show.json", %{permission: permission}) do
     # %{data: render_one(permission, PermissionView, "permission.json")}
-    permission, PermissionView, "permission.json")
+    render_one(permission, PermissionView, "permission.json")
   end
 
   def render("permission.json", %{permission: permission}) do
@@ -18,6 +20,7 @@ defmodule BackendWeb.PermissionView do
       user_id: permission.user_id,
       board_id: permission.board_id
       # user: render_one(permission.user, UserView, "user.json", as: :user),
+      # board: render_one(permission.board, BoardView, "board.json", as: :board),
     }
   end
 end
