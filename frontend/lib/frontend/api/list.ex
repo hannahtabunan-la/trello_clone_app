e77079c9d39b26f6d2bf390665e0eceb60285ad4
hannahtabunan-la <hannah.tabunan@lawadvisor.com>
2021-06-30 23:47:19 +0800

    [F] Added List API and schema

---
+++ frontend/lib/frontend/api/list.ex
@@ -0,0 +1,105 @@
+defmodule Frontend.API.Lists do
+  use Tesla
+
+  alias Ecto.Changeset
+
+  alias Frontend.Schemas.List
+
+  @success_codes 200..399
+  @statuses [
+    :pending,
+    :in_progress,
+    :completed
+  ]
+
+  def change_list(%List{} = list, attrs \\ %{}) do
+    List.changeset(list, attrs)
+  end
+
+  def create_list(params) do
+    IO.puts("+++++ BOARDS_API_CREATE +++++")
+    url = "/lists"
+    {access_token, params} = Map.pop(params, "access_token")
+
+    with %{valid?: true} = changeset <- List.create_changeset(%List{}, params),
+         list <- Changeset.apply_changes(changeset),
+         client <- client(access_token),
+         {:ok, %{body: body, status: status}} when status in @success_codes
+          <- Tesla.post(client, url, %{"list" => list}) do
+      {:ok, from_response(body)}
+    else
+      {:ok, %{body: body}} -> {:error, body}
+      %Changeset{} = changeset -> {:error, changeset}
+      error -> error
+    end
+  end
+
+  def all_lists(params) do
+    url = "/lists"
+    {access_token, _params} = Map.pop(params, "access_token")
+
+    with client <- client(access_token),
+         {:ok, %{body: body, status: status}} when status in @success_codes
+          <- Tesla.get(client, url) do
+      {:ok, Enum.map(body, &from_response/1)}
+    else
+      {:ok, %{body: body}} -> {:error, body}
+      %Changeset{} = changeset -> {:error, changeset}
+      error -> error
+    end
+  end
+
+  def get_list!(params) do
+    url = "/lists/:id"
+    {access_token, params} = Map.pop(params, "access_token")
+
+    with %{valid?: true} = changeset <- List.query_changeset(%List{}, params),
+          query = Changeset.apply_changes(changeset),
+          client <- client(access_token),
+          path_params = Map.take(query, [:id]),
+         {:ok, %{body: body, status: status}} when status in @success_codes
+          <- Tesla.get(client, url, opts: [path_params: path_params]) do
+      {:ok, from_response(body)}
+    else
+      {:ok, %{body: body}} -> {:error, body}
+      %Changeset{} = changeset -> {:error, changeset}
+      error -> error
+    end
+  end
+
+  def update_list(params) do
+    url = "/lists/:id"
+    {access_token, params} = Map.pop(params, "access_token")
+
+    with %{valid?: true} = changeset <- List.changeset(%List{}, params),
+         list = changeset.changes,
+         client <- client(access_token),
+         path_params = Map.take(list, [:id]),
+         {:ok, %{body: body, status: status}} when status in @success_codes
+          <- Tesla.patch(client, url, %{"list" => list}, opts: [path_params: path_params]) do
+      {:ok, from_response(body)}
+    else
+      {:ok, %{body: body}} -> {:error, body}
+      %Changeset{} = changeset -> {:error, changeset}
+      error -> error
+    end
+  end
+
+  defp from_response(response),
+    do: %List{} |> List.changeset(response) |> Changeset.apply_changes()
+
+  def client(access_token) do
+    url = Application.get_env(:frontend, :api_url)
+
+    middleware = [
+      {Tesla.Middleware.BaseUrl, url},
+      Tesla.Middleware.JSON,
+      Tesla.Middleware.PathParams,
+      {Tesla.Middleware.Headers, [{"Authorization", "Bearer #{access_token}"}]},
+    ]
+
+    Tesla.client(middleware)
+  end
+
+  def get_statuses, do: @statuses
+end
---
+++ frontend/lib/frontend/schemas/list.ex
@@ -0,0 +1,56 @@
+defmodule Frontend.Schemas.List do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @schema_fields [
+    :title,
+    :position,
+    :is_deleted,
+    :user_id,
+    :board_id
+  ]
+
+  # @derive {Phoenix.Param, key: :id}
+  @derive {Jason.Encoder, only: @schema_fields}
+
+  @primary_key false
+  schema "lists" do
+    field :is_deleted, :boolean, default: false
+    field :title, :string
+    field :position, :decimal, default: 1.0
+    field :board_id, :id
+    field :user_id, :id
+
+    timestamps()
+  end
+
+  # def schema_fields, do: @schema_fields
+
+  def changeset(struct, params \\ %{}) do
+    cast(struct, params, @schema_fields)
+  end
+
+  def create_changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:title, :position])
+    |> validate_required([:title])
+  end
+
+  def update_changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:title, :position])
+    |> validate_required([:title])
+  end
+
+  def query_changeset(struct, params \\ %{}) do
+    types = %{
+      id: :id,
+      q: :string,
+      limit: :integer,
+      page: :integer
+    }
+
+    {struct, types}
+    |> cast(params, Map.keys(types))
+  end
+end
