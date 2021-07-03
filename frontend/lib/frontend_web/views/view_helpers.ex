2321f07fca8df0ca2b127f74cd625691b2b607d0
hannahtabunan-la <hannah.tabunan@lawadvisor.com>
2021-07-04 03:48:46 +0800

    [F] Update view helpers module

--- frontend/lib/frontend_web/views/view_helpers.ex
+++ frontend/lib/frontend_web/views/view_helpers.ex
@@ -9,4 +9,17 @@ defmodule FrontendWeb.ViewHelpers do
       false -> []
     end
   end
+
+  def get_first_charac_string(""), do: ""
+
+  def get_first_charac_string(nil), do: ""
+
+  def get_first_charac_string(string) do
+    String.split(string)
+    |> Enum.map(&(String.first(&1)))
+    |> List.to_string
+    |> String.upcase
+  end
+
+  def get_current_user_email(conn), do: conn.private.plug_session["user"].email
 end
