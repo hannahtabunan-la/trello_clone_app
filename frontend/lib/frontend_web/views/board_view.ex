1a1fe886dadbb704f5a526556af255e6f2d72c0b
hannahtabunan-la <hannah.tabunan@lawadvisor.com>
2021-07-04 02:11:33 +0800

    [F] Update board table style and data display

--- frontend/lib/frontend_web/views/board_view.ex
+++ frontend/lib/frontend_web/views/board_view.ex
@@ -1,3 +1,9 @@
 defmodule FrontendWeb.BoardView do
   use FrontendWeb, :view
+
+  def permission_label_color(:manage), do: "bg-violet-500 text-violet-900"
+
+  def permission_label_color(:write), do: "bg-violet-300 text-violet-900"
+
+  def permission_label_color(:read), do: "bg-violet-100 text-violet-900"
 end
