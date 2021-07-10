94fa5bf5502617db8f2a9c4eac6319fd3a20fc47
hannahtabunan-la <hannah.tabunan@lawadvisor.com>
2021-07-11 02:23:57 +0800

    [F] Implement loaders and policies

--- frontend/lib/frontend_web.ex
+++ frontend/lib/frontend_web.ex
@@ -23,6 +23,8 @@ defmodule FrontendWeb do
 
       import Plug.Conn
       import FrontendWeb.Gettext
+      alias FrontendWeb.Loaders
+      alias FrontendWeb.Policies
       alias FrontendWeb.Router.Helpers, as: Routes
     end
   end
@@ -36,6 +38,8 @@ defmodule FrontendWeb do
       # Import convenience functions from controllers
       import Phoenix.Controller,
         only: [get_flash: 1, get_flash: 2, view_module: 1, view_template: 1]
+
+      alias FrontendWeb.Policies
 
       # Include shared imports and aliases for views
       unquote(view_helpers())
