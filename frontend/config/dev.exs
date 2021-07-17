1c003fada8c9a46f488f416e3bb044930e94efd5
hannahtabunan-la <hannah.tabunan@lawadvisor.com>
2021-07-18 02:03:14 +0800

    [F] Updated port

--- frontend/config/dev.exs
+++ frontend/config/dev.exs
@@ -7,7 +7,7 @@ use Mix.Config
 # watchers to your application. For example, we use it
 # with webpack to recompile .js and .css sources.
 config :frontend, FrontendWeb.Endpoint,
-  http: [port: 5000],
+  http: [port: 7500],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
